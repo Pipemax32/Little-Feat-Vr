4af6fb5a7703bb2da841fa41743b5e99a8e01328
Pipemax32 <felipeholandes@gmail.com>
2019-08-06 08:28:08 -0300

    Commit inicial

---
+++ Little Feat Vr/Assets/Scripts/ClawSmall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+
+public class ClawSmall : MonoBehaviour {
+
+    public GameObject claw;
+    public OVRInput.Controller controller;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger, controller) > 0.95f && OVRInput.Get(OVRInput.Button.Two, controller) && OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger, controller) < 0.2f)
+        {
+            claw.SetActive(true);
+        }
+        else claw.SetActive(false);
+	}
+}
---
+++ Little Feat Vr/Assets/Scripts/EditSize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditSize : MonoBehaviour {
+    public Vector3 spdScaleEdit;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerStay(Collider other)
+    {
+        if (this.gameObject.tag == "clawSmall")
+        {
+            other.gameObject.transform.localScale -= spdScaleEdit;
+        }
+        if (this.gameObject.tag == "clawBig")
+        {
+            other.gameObject.transform.localScale += spdScaleEdit;
+        }
+    }
+}
